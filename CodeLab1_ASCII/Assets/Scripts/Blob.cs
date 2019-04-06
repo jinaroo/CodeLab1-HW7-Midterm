fc93267f02db0d82201607096cc2ba7889d4c32b
jinaroo <sohnjin@gmail.com>
2019-04-06 18:45:38 -0400

    updated player sprite and added blob enemy functionality

--- CodeLab1_ASCII/Assets/Scripts/Blob.cs
+++ CodeLab1_ASCII/Assets/Scripts/Blob.cs
@@ -5,16 +5,31 @@ using UnityEngine;
 public class Blob : Enemy
 {
     // moves left and right
+    private bool dirRight = true;
+    public float speed = 2.0f;
     
     // Start is called before the first frame update
-    void Start()
+    public override void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (dirRight) // go right
+            transform.Translate (Vector2.right * speed * Time.deltaTime);
+        else // go left
+            transform.Translate (-Vector2.right * speed * Time.deltaTime);
         
+        if(transform.position.x >= .5f)
+        {
+            dirRight = false;
+        }
+        
+        if(transform.position.x <= -.5f)
+        {
+            dirRight = true;
+        }
     }
 }
