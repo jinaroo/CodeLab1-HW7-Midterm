45f0c95a6ea4eb8499835ca34e285759a45428b9
jinaroo <sohnjin@gmail.com>
2019-04-06 19:02:25 -0400

    fixed movement of blob monster

--- CodeLab1_ASCII/Assets/Scripts/Blob.cs
+++ CodeLab1_ASCII/Assets/Scripts/Blob.cs
@@ -6,30 +6,41 @@ public class Blob : Enemy
 {
     // moves left and right
     private bool dirRight = true;
-    public float speed = 2.0f;
+
+    public float speed = .5f;
+    public float pathWidth = 2.0f;
+
+    public Vector3 rightEnd;
+    public Vector3 leftEnd;
     
     // Start is called before the first frame update
     public override void Start()
     {
         base.Start();
+
+        rightEnd = transform.localPosition + Vector3.right * (pathWidth / 2);
+        leftEnd = transform.localPosition + Vector3.left * (pathWidth / 2);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (dirRight) // go right
-            transform.Translate (Vector2.right * speed * Time.deltaTime);
-        else // go left
-            transform.Translate (-Vector2.right * speed * Time.deltaTime);
-        
-        if(transform.position.x >= .5f)
+        if (dirRight)
         {
-            dirRight = false;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, rightEnd, speed * Time.deltaTime);
+            if (transform.localPosition == rightEnd)
+            {
+                dirRight = false;
+            }
         }
-        
-        if(transform.position.x <= -.5f)
+        else
         {
-            dirRight = true;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, leftEnd, speed * Time.deltaTime);
+            if (transform.localPosition == leftEnd)
+            {
+                dirRight = true;
+            }
         }
+               
     }
 }
