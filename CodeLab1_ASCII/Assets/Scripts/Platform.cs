76a04c0bff1fff6c8ad4f27ca3acca1b0a58adf8
jinaroo <sohnjin@gmail.com>
2019-04-06 21:24:51 -0400

    added title screen and expanded on level

--- CodeLab1_ASCII/Assets/Scripts/Platform.cs
+++ CodeLab1_ASCII/Assets/Scripts/Platform.cs
@@ -1,22 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
+public class Platform : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<Animator>().Play("platform_animation", -1, Random.value);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.R) || Input.GetKeyDown(KeyCode.Return))
-        {
-            SceneManager.LoadScene(0);
-        }
+        
     }
 }
